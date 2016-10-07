2ff788a81d9e29a59f035ecb8ac70d4e531ee02d
Andreas Nilsson <an@bahnhof.se>
2016-10-08 00:53:51 +0200

    Fetch QCC with script.

---
+++ updatebinaries.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+QCC=../fteqcc
+SRV=../fteqw.sv
+
+if [ `getconf LONG_BIT` = "64" ]; then
+	wget -O ${QCC} http://triptohell.info/moodles/linux_amd64/fteqcc64
+	wget -O ${SRV} http://triptohell.info/moodles/linux_amd64/fteqw-sv64
+else
+	wget -O ${QCC} http://triptohell.info/moodles/linux_x86/fteqcc32
+	wget -O ${SRV} http://triptohell.info/moodles/linux_x86/fteqw-sv32
+fi
+
+chmod +x ${QCC}
+chmod +x ${SRV}
