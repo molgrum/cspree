64f48f1549252e47947c380cbe58a9565e5ae6cb
Andreas Nilsson <an@bahnhof.se>
2016-10-09 01:07:31 +0200

    For the Windows folks.

---
+++ winquicker.bat
@@ -0,0 +1,12 @@
+cd ..
+IF EXIST "%PROGRAMFILES(x86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+
+:64BIT
+fteqwsv64.exe -game gmapn +exec ports/port1.cfg
+GOTO END
+
+:32BIT
+fteqwsv32.exe -game gmapn +exec ports/port1.cfg
+GOTO END
+
+:END
