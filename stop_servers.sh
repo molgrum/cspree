e49e527fd2583738249406d236e688f09ad67876
Andreas Nilsson <an@bahnhof.se>
2016-10-21 13:38:05 +0200

    Bug fix.

--- stop_servers.sh
+++ stop_servers.sh
@@ -3,25 +3,25 @@
 # Kill 28501
 pid=`ps ax | grep -v grep | grep "/bin/sh ./run/port1.sh" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
-pid=`ps ax | grep -v grep | grep "fteqw.sv -game gmapn +exec ports/port1.cfg" | awk '{print $1}'`
+pid=`ps ax | grep -v grep | grep "fteqw.sv -game cspree +exec ports/port1.cfg" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
 
 # Kill 28502
 pid=`ps ax | grep -v grep | grep "/bin/sh ./run/port2.sh" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
-pid=`ps ax | grep -v grep | grep "fteqw.sv -game gmapn +exec ports/port2.cfg" | awk '{print $1}'`
+pid=`ps ax | grep -v grep | grep "fteqw.sv -game cspree +exec ports/port2.cfg" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
 
 # Kill 28503
 pid=`ps ax | grep -v grep | grep "/bin/sh ./run/port3.sh" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
-pid=`ps ax | grep -v grep | grep "fteqw.sv -game gmapn +exec ports/port3.cfg" | awk '{print $1}'`
+pid=`ps ax | grep -v grep | grep "fteqw.sv -game cspree +exec ports/port3.cfg" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
 
 # Kill 28504
 pid=`ps ax | grep -v grep | grep "/bin/sh ./run/port4.sh" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
-pid=`ps ax | grep -v grep | grep "fteqw.sv -game gmapn +exec ports/port4.cfg" | awk '{print $1}'`
+pid=`ps ax | grep -v grep | grep "fteqw.sv -game cspree +exec ports/port4.cfg" | awk '{print $1}'`
 if [ "$pid" != "" ]; then kill -9 $pid; fi;
 
 # Kill QTV
