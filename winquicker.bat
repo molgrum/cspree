c7d4a4125ce4a7f2234485268b3a8e81b4f52e30
Andreas Nilsson <an@bahnhof.se>
2016-11-04 21:11:07 +0100

    Small bugfix for windows people.

--- winquicker.bat
+++ winquicker.bat
@@ -2,11 +2,11 @@ cd ..
 IF EXIST "%PROGRAMFILES(x86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
 
 :64BIT
-fteqwsv64.exe -game gmapn +exec ports/port1.cfg
+fteqwsv64.exe -game cspree +exec ports/port1.cfg
 GOTO END
 
 :32BIT
-fteqwsv32.exe -game gmapn +exec ports/port1.cfg
+fteqwsv32.exe -game cspree +exec ports/port1.cfg
 GOTO END
 
 :END
