1b12b67c85b1b254d8bd617e79b1d3fea8c5ef3e
Andreas Nilsson <an@bahnhof.se>
2017-03-05 20:01:27 +0100

    updatebinaries.sh: Wget flags changed and a generally more clean experience

--- updatebinaries.sh
+++ updatebinaries.sh
@@ -1,18 +1,80 @@
 #!/bin/sh
 
+URL=http://triptohell.info/moodles
+
+LINUX32_DIR=linux_x86
+LINUX32_QCC=fteqcc32
+LINUX32_FTE=fteqw-sv32
+
+LINUX64_DIR=linux_amd64
+LINUX64_QCC=fteqcc64
+LINUX64_FTE=fteqw-sv64
+
+PAK_DIR=junk
+PAK_BIN=cspree.pk3
+
 QCC=./fteqcc
-SRV=../fteqw.sv
-PAK=gmapn.pk3
+FTE=../fteqw.sv
+
+download() {
+	printf "Downloading $1..."
+
+	# To compare the current version with the new
+	if [ -f $3 ]; then
+		MD5="$(md5sum $3 | awk '{print $1}')"
+	fi
+
+	if wget --output-file wgetlog --server-response --timestamping ${URL}/$2/$3; then
+		printf " Done."
+		if [ "$MD5" = "$(md5sum $3 | awk '{print $1}')" ]; then
+			printf " File is already the latest version.\n"
+			DOWNLOAD_OK=0
+		else
+			printf "\n"
+			DOWNLOAD_OK=1
+		fi
+	else
+		printf " Failed! From wgetlog:\n"
+		cat wgetlog
+		DOWNLOAD_OK=0
+	fi
+}
+
+permissions() {
+	printf "Setting executable permissions for $1.\n"
+	if chmod +x $2; then
+		PERMISSIONS_OK=1
+	else
+		PERMISSIONS_OK=0
+	fi
+}
+
+symlink() {
+	if [ -f $2 ] && [ ! -L $2 ]; then
+		printf "$2 is a regular file, answer yes to change it to a symlink.\n"
+		ln --interactive -s ${PWD}/$3 $2
+	elif [ ! -L $2 ]; then
+		printf "Creating symlink for $1.\n"
+		ln -s ${PWD}/$3 $2
+	fi
+}
+
+runonce() {
+	download $1 $2 $3
+	if [ $DOWNLOAD_OK -ne 0 ]; then
+		permissions $1 $3
+		if [ $PERMISSIONS_OK -ne 0 ]; then
+			symlink $1 $4 $3
+		fi
+	fi
+}
 
 if [ `getconf LONG_BIT` = "64" ]; then
-	wget -O ${QCC} http://triptohell.info/moodles/linux_amd64/fteqcc64
-	wget -O ${SRV} http://triptohell.info/moodles/linux_amd64/fteqw-sv64
+	runonce QCC ${LINUX64_DIR} ${LINUX64_QCC} ${QCC}
+	runonce FTE ${LINUX64_DIR} ${LINUX64_FTE} ${FTE}
 else
-	wget -O ${QCC} http://triptohell.info/moodles/linux_x86/fteqcc32
-	wget -O ${SRV} http://triptohell.info/moodles/linux_x86/fteqw-sv32
+	runonce QCC ${LINUX32_DIR} ${LINUX32_QCC} ${QCC}
+	runonce FTE ${LINUX32_DIR} ${LINUX32_FTE} ${FTE}
 fi
 
-chmod +x ${QCC}
-chmod +x ${SRV}
-
-wget -O ${PAK} http://triptohell.info/moodles/junk/${PAK}
+download PK3 ${PAK_DIR} ${PAK_BIN}
