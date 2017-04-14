634e52dd458331aad1eb2847bf9a2ae7d901d3eb
Andreas Nilsson <an@bahnhof.se>
2017-04-14 16:41:33 +0200

    SCRIPTS: stop_servers.sh uses SIGTERM now, waits for success

--- stop_servers.sh
+++ stop_servers.sh
@@ -3,10 +3,37 @@
 port=27511
 
 while [ -f ./run/port$port.sh ]; do
+	timeout=false
+	printf "* Stopping ftesv (port %s)..." $port
 	pid=`ps ax | grep -v grep | grep "/bin/sh ./run/port$port.sh" | awk '{print $1}'`
-	if [ "$pid" != "" ]; then kill -9 $pid; fi;
+	if [ "$pid" != "" ]; then
+		count=0
+		while kill $pid 2> /dev/null; do
+			if [ $count -eq 10 ]; then
+				timeout=true
+				break
+			fi
+			sleep 1
+			let count=count+1
+		done
+	fi
 	pid=`ps ax | grep -v grep | grep "fteqw.sv -game cspree -port $port +set port $port" | awk '{print $1}'`
-	if [ "$pid" != "" ]; then kill -9 $pid; fi;
+	if [ "$pid" != "" ]; then
+		count=0
+		while kill $pid 2> /dev/null; do
+			if [ $count -eq 10 ]; then
+				timeout=true
+				break
+			fi
+			sleep 1
+			let count=count+1
+		done
+	fi
+	if [ timeout == true ]; then
+		echo "[TIMED OUT]"
+	else
+		echo "[OK]"
+	fi
 	let port=port+1
 done
 
